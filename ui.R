b48d6b6baaec299156952f272e79f117b71f29a8
Davi Carvalho <davi.carvalho@gmail.com>
2016-04-17 23:47:38 -0300

    Added files via upload

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(
     sidebarPanel(
       h5("This application shows a graph that has on y-axis the number of families that were assisted by a CRAS (Social Assistance Reference Center) and on x-axis the number of workers of that CRAS."),
       h5("There is two kinds of filters that you can use. On the first filter you select the state you want to see on the plot and the second filter you can choose the size of the cities which the CRAS are located."),
-      h5("The graph shows two red lines on the means of y-axis and x-axis."),
+      h5("The graph shows two red lines on the means of y-axis and x-axis. And each size of city has a line that show the linear regression of points plotted."),
       h1("Filter"),
       selectInput("uf", "Brazilian State:",
                   c('BR',
